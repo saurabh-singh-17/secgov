1195a65b9f8c643e59788351c75cd68f99a8fde3
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2016-06-14 13:32:35 +0530

    cchanges

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ library(data.table)
 library(scales)
 
 #reading the data
-data<-fread("C:/Users/u472290/workfolder/shiny_capability2/Consumer_Complaints.csv",sep=",")
+data<-fread("C:/Users/saurabh/Desktop/shiny/Consumer_Complaints.csv",sep=",")
 
 #changing the column names to remove special characters
 setnames(data,colnames(data),gsub("[^[:alnum:]]","_",colnames(data)))
@@ -21,14 +21,20 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("company",label = "Select the Bank", choices = unique(data$Company)),
-      selectInput("product",label = "Select the Bank", choices = unique(data$Product))
+      selectInput("product",label = "Select the Product", choices = unique(data$Product)),
+      selectInput("subproduct",label = "Select the Sub-Product", choices = unique(data$Sub_product)),
+      dateRangeInput("daterange",label="Select the Date", start=min(data$Date_received),end=max(data$Date_received))
        ),
     
     # Show a table summarizing the values entered
     mainPanel(
       tabsetPanel(type = "tabs", 
                   tabPanel("Product Complaints", plotOutput("productReport")),
-                  tabPanel("Sub-Product Complaints", plotOutput("subproductReport"))
+                  tabPanel("Sub-Product Complaints", plotOutput("subproductReport")),
+                  tabPanel("Issues with the Sub-Product", plotOutput("issuesubproductReport")),
+                  tabPanel("Complaints Region", plotOutput("region")),
+                  tabPanel("Overall Product Complaints", plotOutput("overallproductReport")),
+                  tabPanel("Overall Complaints Region", plotOutput("overallregion"))
       )
     )
                   
